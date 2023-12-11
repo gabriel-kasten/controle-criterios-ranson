b79a2f091eb6f4bf1ccd2cc7747bf58b5f312d85
gabriel kasten <gabrieldegkasten@gmail.com>
2023-12-11 00:20:09 -0300

    first version

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,219 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Critérios de Ranson',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PatientForm(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PatientForm extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PatientFormState createState() => _PatientFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PatientFormState extends State<PatientForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool isPancreatitisGrave = false;
+  int score = 0;
+  double mortality = 0.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController leukocytesController = TextEditingController();
+  TextEditingController glucoseController = TextEditingController();
+  TextEditingController astController = TextEditingController();
+  TextEditingController ldhController = TextEditingController();
+  bool hasGallstones = false;
+
+  void calculateScore() {
+    int age = int.parse(ageController.text);
+    int leukocytes = int.parse(leukocytesController.text);
+    double glucose = double.parse(glucoseController.text);
+    double ast = double.parse(astController.text);
+    double ldh = double.parse(ldhController.text);
+
+    int thresholdAge = hasGallstones ? 70 : 55;
+    int thresholdLeukocytes = hasGallstones ? 18000 : 16000;
+    double thresholdGlucose = hasGallstones ? 12.2 : 11.0;
+    double thresholdAst = 250.0;
+    double thresholdLdh = hasGallstones ? 400.0 : 350.0;
+
+    score = 0;
+
+    if (age > thresholdAge) score++;
+    if (leukocytes > thresholdLeukocytes) score++;
+    if (glucose > thresholdGlucose) score++;
+    if (ast > thresholdAst) score++;
+    if (ldh > thresholdLdh) score++;
+
+    isPancreatitisGrave = score >= 3;
+
+    if (score >= 0 && score <= 2) {
+      mortality = 2.0;
+    } else if (score >= 3 && score <= 4) {
+      mortality = 15.0;
+    } else if (score >= 5 && score <= 6) {
+      mortality = 40.0;
+    } else {
+      mortality = 100.0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: const Text('Novo paciente'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o nome do paciente.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: ageController,
+                decoration: const InputDecoration(labelText: 'Idade'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira a idade do paciente.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: leukocytesController,
+                decoration: const InputDecoration(labelText: 'Leucócitos'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o número de leucócitos.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: glucoseController,
+                decoration: const InputDecoration(labelText: 'Glicemia'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o valor da glicemia.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: astController,
+                decoration: const InputDecoration(labelText: 'AST/TGO'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o valor de AST/TGO.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: ldhController,
+                decoration: const InputDecoration(labelText: 'LDH'),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o valor de LDH.';
+                  }
+                  return null;
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('Litíase Biliar'),
+                value: hasGallstones,
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() {
+                      hasGallstones = value;
+                    });
+                  }
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    calculateScore();
+                    _showResultDialog();
+                  }
+                },
+                child: const Text('Calcular'),
+              ),
+              SizedBox(height: 20),
+              Visibility(
+                visible: score > 0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Pontuação: $score'),
+                    Text('Mortalidade: $mortality%'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _showResultDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Resultado'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pontuação: $score'),
+              Text('Mortalidade: $mortality%'),
+              Text(
+                isPancreatitisGrave
+                    ? 'Pancreatite Grave'
+                    : 'Pancreatite não Grave',
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
